03d9a5ef07342c49900bde2ff478fd3865b8cb57
Tuấn Kiệt( 22T1020183) <117705056+TUANKIETPHANHUU@users.noreply.github.com>
2026-04-03 14:04:11 +0700

    f

--- src/data_utils.py
+++ src/data_utils.py
@@ -54,12 +54,13 @@ def draw_labels_and_boxes(image, labels, boxes):
 
 def get_output_layers(model):
     layers_name = model.getLayerNames()
-    try:
-        # Cập nhật cho OpenCV bản mới trên Streamlit Cloud
-        output_layers = [layers_name[i - 1] for i in model.getUnconnectedOutLayers()]
-    except Exception:
-        # Dự phòng cho OpenCV bản cũ nếu bạn chạy code dưới máy local
-        output_layers = [layers_name[i[0] - 1] for i in model.getUnconnectedOutLayers()]
+    out_layers = model.getUnconnectedOutLayers()
+    
+    # "Nghiền phẳng" mọi loại mảng (1 chiều hay 2 chiều) về một danh sách các con số
+    out_layers_flat = np.array(out_layers).flatten()
+    
+    # Dùng int(i) để ép kiểu, đảm bảo không bao giờ bị lỗi 'scalar variable' nữa
+    output_layers = [layers_name[int(i) - 1] for i in out_layers_flat]
 
     return output_layers
 
@@ -79,7 +80,7 @@ def order_points(coordinates):
 
 def convert2Square(image):
     """
-    Resize non square image(height != width to square one (height == width)
+    Resize non square image(height != width) to square one (height == width)
     :param image: input images
     :return: numpy array
     """
